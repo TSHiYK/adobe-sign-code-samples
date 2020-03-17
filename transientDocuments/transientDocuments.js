117ee86287f0940432762a275bdf09ec8782ce87
Toshiyuki Ichige <TSHiYK@users.noreply.github.com>
2020-03-17 14:01:24 +0900

    feat: add  transientDocuments.js

---
+++ transientDocuments/transientDocuments.js
@@ -0,0 +1,36 @@
+/**
+ * POST /transientDocuments
+ * 
+ * Uploads a document and obtains the document's ID.
+ * ドキュメントファイルをアップロードし、transientDocumentId を取得します。
+ * 
+ * https://secure.na1.echosign.com/public/docs/restapi/v6#!/transientDocuments/createTransientDocument
+ */
+
+require('dotenv').config();
+const request = require("request");
+const fs = require("fs");
+
+const options = {
+    method: "POST",
+    url: `${process.env.api_access_point}/api/rest/v6/transientDocuments`,
+    headers: {
+        "Authorization": `Bearer ${process.env.access_token}`,
+        "Content-Type": "multipart/form-data"
+    },
+    formData: {
+        "File": fs.createReadStream("./sample.pdf")
+    }
+};
+
+exports.postTransientDocuments = () => {
+    return new Promise((resolve, reject) => {
+        request.post(options, (error, response, body) => {
+            if (error) {
+                console.log(error);
+            }
+
+            resolve(body);
+        });
+    });
+}
